6b3de762c3fda1d0a6d9fbf4a16d0c30a7827de4
Edward Jacobs <eddielawrence23@gmail.com>
2019-11-27 17:07:33 +0100

    Add reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+    Restaurant.destroy_all
+
+    Restaurant.create(name: 'Tickets', address: 'Barcelona', category: 'italian', phone_number: '34 000 00 00')
+    Restaurant.create(name: 'Can Ros', address: 'Barcelona', category: 'italian', phone_number: '34 000 00 00')
+    Restaurant.create(name: 'Subway', address: 'Barcelona', category: 'italian', phone_number: '34 000 00 00')
+    Restaurant.create(name: 'Yoma', address: 'Barcelona', category: 'italian', phone_number: '34 000 00 00')
+    Restaurant.create(name: 'Gonzalez', address: 'Barcelona', category: 'italian', phone_number: '34 000 00 00')
+
+
+    p "#{Restaurant.count} # of restaurants created"
